1a10812d1de579b06e37278bbf0289e548ee0710
Artina Ferizi <tina.fe12@hotmail.com>
2016-04-09 20:28:31 +0200

    Create Doublet.java

---
+++ Doublet.java
@@ -0,0 +1,15 @@
+class Doublet
+{
+	public static void main (String args[])
+	{
+		double t;
+		t = Double.parseDouble(args[0]);
+		
+		System.out.println(Math.sin(2*t)+Math.sin(3*t));
+	}
+}
+
+/*
+Exercise: 1.2.21
+Book: Introduction to Programming in Java: An Interdisciplinary Approach
+*/ 
